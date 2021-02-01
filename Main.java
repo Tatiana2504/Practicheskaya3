7cc46af7daf3054fb5938d7b969a36ab4b897aa2
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-02-01 18:21:54 +0300

    zadanie4

--- Main.java
+++ Main.java
@@ -4,36 +4,40 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Введите день недели: ");
         String s = reader.readLine();
-
-        switch (s) {
-            case "понедельник":
-                System.out.println("1");
-                break;
-            case "вторник":
-                System.out.println("2");
-                break;
-            case "среда":
-                System.out.println("3");
-                break;
-            case "четверг":
-                System.out.println("4");
-                break;
-            case "пятница":
-                System.out.println("5");
-                break;
-            case "суббота":
-                System.out.println("6");
-                break;
-            case "воскресенье":
-                System.out.println("7");
-                break;
-
-            default:
-                System.out.println("недопустимое значение");
-
+        int n = Integer.parseInt(s);
+        if (n < 0)
+        {
+            System.out.println("недопустимое значение");
+        }
+        else if (n < 51)
+        {
+            System.out.println("Разряд F");
+        }
+        else if (n < 61 )
+        {
+            System.out.println("Разряд E");
+        }
+        else if (n < 71)
+        {
+            System.out.println("Разряд D");
+        }
+        else if (n < 81)
+        {
+            System.out.println("Разряд C");
+        }
+        else if (n < 91)
+        {
+            System.out.println("Разряд B");
         }
 
+        else if (n <= 100)
+        {
+            System.out.println("Разряд A");
+        }
+        else
+        {
+            System.out.println("недопустимое значение");
+        }
     }
 }
