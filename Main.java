06a85de7fa1d7175d4c0ccb7ff03f8a2ca3f7109
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-02-02 14:51:00 +0300

    zadanie5.2

--- Main.java
+++ Main.java
@@ -2,19 +2,25 @@ package com.metanit;
 import static java.lang.Math.*;
 public class Main {
     public static void main(String[] args) {
-        double a = 7.5;
-        double b = 1.4;
-        double x1 = -4.13;
-        double x2 = 0.77;
+        double a = 7.2;
+        double b = 3.9;
+        double x1 = -0.38;
+        double x2 = 4.19;
+        double x3 = 9.13;
         double y1 = 0;
         double y2 = 0;
-        y1 = a - b * b * x1;
-        y2 = 1 / (x2 * x2 + exp(Math.log(a)));
-        if (x1 <= -3) {
-            System.out.print("y1 = " + y1 + "\n");
+        double y3 = 0;
+        y1 = a*x1 + b*x1*x1;
+        y2 = exp(x2) + x2*x2;
+        y3 = sin(b*x3);
+        if (x1<3) {
+            System.out.print(y1);
         }
-        if (x2 > -3) {
-            System.out.print("y2 = " + y2 + "\n");
+        else if (x2>= 3 & x2<=6){
+            System.out.print(y2);
+        }
+        else if (x3>6){
+            System.out.print(y3);
         }
     }
-    }
+}
