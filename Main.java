e4a5614f71c79a963e626fb603a2f126ded2f0b5
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-02-13 13:37:13 +0300

    Pz4-5(1)

--- Main.java
+++ Main.java
@@ -1,26 +1,80 @@
 package com.metanit;
-import static java.lang.Math.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        double a = 7.2;
-        double b = 3.9;
-        double x1 = -0.38;
-        double x2 = 4.19;
-        double x3 = 9.13;
-        double y1 = 0;
-        double y2 = 0;
-        double y3 = 0;
-        y1 = a*x1 + b*x1*x1;
-        y2 = exp(x2) + x2*x2;
-        y3 = sin(b*x3);
-        if (x1<3) {
-            System.out.print(y1);
+        String nums[];
+        int a = -1;
+        String v;
+        int p;
+        nums = new String[1];
+        nums = Put("0", nums, a = in(a));
+        nums = Put("4", nums, a = in(a));
+        nums = Put("8", nums, a = in(a));
+        nums = Put("5", nums, a = in(a));
+        nums = Put("9", nums, a = in(a));
+
+        nums = Put("6", nums, a = in(a));
+        nums = Put("485", nums, a = in(a));
+        nums = Put("2", nums, a = in(a));
+        nums = Put("9", nums, a = in(a));
+
+        System.out.println("_____________");
+
+        v = Get(nums);
+        nums = Gets(nums);
+        System.out.println(v);
+        v = Get(nums);
+        nums = Gets(nums);
+        System.out.println(v);
+
+        System.out.println("_____________");
+        System.out.println(nums.length);
+        nums = AllGets(nums);
+
+        System.out.println(nums.length);
+
+        System.out.println("_____________");
+
+        int W[] = new int[nums.length];
+
+        for (p = 0; p < nums.length; p++) {
+
+            W[p] = Integer.parseInt(nums[p]);
         }
-        else if (x2>= 3 & x2<=6){
-            System.out.print(y2);
+        W = insertionSortImperative(W);
+
+        for (p = 0; p < W.length; p++) {
+            System.out.println(W[p]);
         }
-        else if (x3>6){
-            System.out.print(y3);
+        System.out.println("_____________СОРТИРОВКА_____________");
+
+        String c[] = new String[2];
+        int max = 0, min = 30;
+        double B;
+        String m;
+        for (p = 0; p < 10; p++) {
+            B = Math.random() * 30;
+            m = (int) a + "";
+            if (a > max) {
+                max = p;
+            }
+            if (a < min) {
+                min = p;
+            }
+
+            c = Put(m, c, p);
+        }
+
+        String s;
+        s = c[max];
+        c[max] = c[min];
+        c[min] = s;
+
+        System.out.println("_____________ВЫВОД_____________");
+        for (p = 0; p < 10; p++) {
+            System.out.println(c[p]);
+
         }
     }
 }
